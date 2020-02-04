c690f797191efc100b8bce613cf3eae6b40b853f
“Dean <deanhardy@gmail.com>
2020-02-04 13:38:00 -0500

    added variables

--- scripts/slr-typology.R
+++ scripts/slr-typology.R
@@ -10,79 +10,93 @@ library(ggplot2)
 datadir <- '/Users/dhardy/Dropbox/r_data/web-of-science' 
   
 ## import data
-all <- read.delim(file.path(datadir, "data/slr-risk.txt"), header = TRUE, sep = "\t") %>%
+slr <- read.delim(file.path(datadir, "data/slr.txt"), header = TRUE, sep = "\t") %>%
   head(., -2) %>%
   mutate(year = as.character(Publication.Years)) %>%
-  mutate(year = year(as.Date(year, "%Y"))) %>%
-  select("year", "records")
+  mutate(year = year(as.Date(year, "%Y")), id = 'slr') %>%
+  select("year", "records", 'id')
 
-# vulnhd <- read.delim("data/vulnhd.txt", header = TRUE, sep = "\t") %>%
-#   head(., -2) %>%
-#   mutate(count = records, year = as.character(?..Publication.Years)) %>%
-#   mutate(year = year(as.Date(year, "%Y"))) %>%
-#   select("year", "count")
+risk <- read.delim(file.path(datadir, "data/slr-risk.txt"), header = TRUE, sep = "\t") %>%
+  head(., -2) %>%
+  mutate(year = as.character(Publication.Years)) %>%
+  mutate(year = year(as.Date(year, "%Y")), id = 'risk') %>%
+  select("year", "records", 'id')
 
-# df <- full_join(vuln, vulnhd, by = "year") %>%
-#   rename(vuln = count.x, vulnhd = count.y) %>%
-#   gather(key = identity, value = records, vuln, vulnhd) %>%
-#   group_by(identity) %>%
-#   arrange(year)
+pop <- read.delim(file.path(datadir, "data/slr-pop.txt"), header = TRUE, sep = "\t") %>%
+  head(., -2) %>%
+  mutate(year = as.character(Publication.Years)) %>%
+  mutate(year = year(as.Date(year, "%Y")), id = 'pop') %>%
+  select("year", "records", 'id')
 
-fig <- ggplot(filter(all, year <= 2020 & year > 1964), aes(year, records/100)) + 
-  geom_point(aes(), size = 2, color = "black") + 
-  scale_y_continuous(name = "Records (100s)", limits = c(0,18), breaks = seq(0, 16, 2),
-                     sec.axis = sec_axis(~., breaks = seq(0,18,2), labels = NULL)) + 
-  scale_x_continuous(name = "Year", breaks = seq(1970, 2020, 10),
-                     sec.axis = sec_axis(~., breaks = seq(1970,2020, 10), labels = NULL)) + 
-  scale_shape_manual(name = "", values = c(18,1), labels = c("Sea level rise", "Risk")) + 
-  theme(legend.position = c(0.35,0.85),
-        legend.key = element_rect(fill = "white"),
-        legend.background = element_rect(fill = "white"),
-        panel.background = element_rect(fill = "white", color = "white"),
-        panel.grid = element_blank(),
-        plot.background = element_rect(fill = "white", color = "white"),
-        axis.line = element_line(color = "black"),
-        axis.text = element_text(color = "black"),
-        axis.text.x = element_text(margin = unit(c(0.5,0.5,0.5,0.5), "cm")),
-        axis.text.y = element_text(margin = unit(c(0.5,0.5,0.5,0.5), "cm")),
-        axis.ticks.length = unit(-0.15, "cm"),
-        axis.ticks = element_line(color = "black"),
-        text = element_text(size = 12, color = "black"),
-        plot.margin = margin(1,1,0.5,0.5, "cm")
-        )
-fig
+poprisk <- read.delim(file.path(datadir, "data/slr-pop-risk.txt"), header = TRUE, sep = "\t") %>%
+  head(., -2) %>%
+  mutate(year = as.character(Publication.Years)) %>%
+  mutate(year = year(as.Date(year, "%Y")), id = 'pop+risk') %>%
+  select("year", "records", 'id')
 
-tiff('figures/vuln.tiff', width=5, height=5, units="in", res=300, compression = "lzw")
-fig
-dev.off()
+# slr <- slr[1,] %>%
+#   mutate(year = NA, records = NA, id = 'slr')
+
+all <- rbind(risk, pop, poprisk, slr) %>%
+  mutate(id = factor(id, levels = c('slr', 'risk', 'pop', 'pop+risk')))
+
+# fig <- ggplot(all, aes(year, records, color = id)) + 
+#   geom_point(size = 2) + 
+#   scale_y_continuous(name = "Records", limits = c(0,200), breaks = seq(0, 200, 20),
+#                      sec.axis = sec_axis(~., breaks = seq(0,200,20), labels = NULL)) + 
+#   scale_x_continuous(name = "Year", breaks = seq(1990, 2020, 5),
+#                      sec.axis = sec_axis(~., breaks = seq(1990,2020, 5), labels = NULL)) + 
+#  # scale_shape_manual(name = "", values = c(180,1), labels = c("Sea level rise", "Risk")) + 
+#   theme(legend.position = c(0.35,0.85),
+#         legend.key = element_rect(fill = "white"),
+#         legend.background = element_rect(fill = "white"),
+#         panel.background = element_rect(fill = "white", color = "white"),
+#         panel.grid = element_blank(),
+#         plot.background = element_rect(fill = "white", color = "white"),
+#         axis.line = element_line(color = "black"),
+#         axis.text = element_text(color = "black"),
+#         axis.text.x = element_text(margin = unit(c(0.5,0.5,0.5,0.5), "cm")),
+#         axis.text.y = element_text(margin = unit(c(0.5,0.5,0.5,0.5), "cm")),
+#         axis.ticks.length = unit(-0.15, "cm"),
+#         axis.ticks = element_line(color = "black"),
+#         text = element_text(size = 12, color = "black"),
+#         plot.margin = margin(1,1,0.5,0.5, "cm")
+#         )
+# fig
+# 
+# tiff('figures/slr-pop-risk.tiff', width=5, height=5, units="in", res=300, compression = "lzw")
+# fig
+# dev.off()
 
 
-fig <- ggplot(df, aes(year, records/1000), group = identity) + 
-  geom_point(aes(shape = identity), size = 5, color = "white") +
-  scale_shape_manual(name = "", values = c(18,1), labels = c("Vulnerability", "AND (Hazards OR Disasters)")) +
+fig <- ggplot(all, aes(year, records), group = id) + 
+  geom_point(aes(shape = id), size = 2) +
+  scale_shape_manual(name = "", values = c(1,18,8,2), labels = c("Sea-Level Rise\nOR Sea Level Rise", "AND Risk", "AND Population",
+                                                                "AND Population AND Risk")) +
   # geom_smooth(method = "loess", se = FALSE, lwd = 1.2, aes(color = identity)) +
   # scale_color_manual(name = "", values = c("steelblue1", "tomat3"), labels = c("Vulnerability", "AND (Hazards OR Disasters)")) +
-  scale_y_continuous(name = "Records (1000s)", limits = c(0,12), breaks = seq(0, 12, 2),
-                     sec.axis = sec_axis(~., breaks = seq(0,12,2), labels = NULL)) + 
-  scale_x_continuous(name = "Year", breaks = seq(1945, 2020, 10), 
-                     sec.axis = sec_axis(~., breaks = seq(1945,2020, 10), labels = NULL)) + 
-  theme(legend.position = c(0.45,0.85),
+  scale_y_continuous(name = "No. of Records", limits = c(0,320), breaks = seq(0, 320, 20),
+                     sec.axis = sec_axis(~., breaks = seq(0,320,20), labels = NULL)) + 
+  scale_x_continuous(name = "Year", breaks = seq(1990, 2020, 5), 
+                     sec.axis = sec_axis(~., breaks = seq(1990,2020, 5), labels = NULL)) + 
+  theme(legend.position = c(0.25,0.85),
         legend.key = element_rect(fill = "transparent", color = "transparent"),
         legend.background = element_rect(fill = "transparent"),
         panel.background = element_rect(fill = "transparent", color = "transparent"),
         panel.grid = element_blank(),
         plot.background = element_rect(fill = "transparent", color = "transparent"),
-        axis.line = element_line(color = "white"),
-        axis.text = element_text(color = "white"),
+        axis.line = element_line(color = "black"),
+        axis.text = element_text(color = "black"),
         axis.text.x = element_text(margin = unit(c(0.5,0.5,0.5,0.5), "cm")),
         axis.text.y = element_text(margin = unit(c(0.5,0.5,0.5,0.5), "cm")),
-        axis.ticks.length = unit(-0.15, "cm"),
-        axis.ticks = element_line(color = "white"),
-        text = element_text(size = 20, color = "white"),
+        axis.ticks.length = unit(-0.1, "cm"),
+        axis.ticks = element_line(color = "black"),
+        text = element_text(size = 10, color = "black"),
         plot.margin = margin(1,1,0.5,0.5, "cm")
   )
 fig
-png('figures/vuln_slide.png', width=6, height = 6, units="in", res = 150, bg = "transparent")
+
+tiff(file.path(datadir, '/figures/slr-pop-risk.tiff'), width=5, height=5, units="in", res = 300, compression = 'lzw')
 fig
 dev.off()
 
