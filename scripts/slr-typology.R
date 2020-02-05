e46158ce7a769f9078ec236b6462d20d20f8e787
“Dean <deanhardy@gmail.com>
2020-02-05 12:44:23 -0500

    added overlaid columns with legend but still need to reorder legend

--- scripts/slr-typology.R
+++ scripts/slr-typology.R
@@ -38,50 +38,57 @@ poprisk <- read.delim(file.path(datadir, "data/slr-pop-risk.txt"), header = TRUE
 #   mutate(year = NA, records = NA, id = 'slr')
 
 all <- rbind(risk, pop, poprisk, slr) %>%
-  mutate(id = factor(id, levels = c('slr', 'risk', 'pop', 'pop+risk')))
+  #mutate(id = factor(id, levels = c('slr', 'risk', 'pop', 'pop+risk'))) %>%
+  mutate(id = factor(id, levels = c('pop+risk', 'risk', 'pop', 'slr')))
 
-# fig <- ggplot(all, aes(year, records, color = id)) + 
-#   geom_point(size = 2) + 
-#   scale_y_continuous(name = "Records", limits = c(0,200), breaks = seq(0, 200, 20),
-#                      sec.axis = sec_axis(~., breaks = seq(0,200,20), labels = NULL)) + 
-#   scale_x_continuous(name = "Year", breaks = seq(1990, 2020, 5),
-#                      sec.axis = sec_axis(~., breaks = seq(1990,2020, 5), labels = NULL)) + 
-#  # scale_shape_manual(name = "", values = c(180,1), labels = c("Sea level rise", "Risk")) + 
-#   theme(legend.position = c(0.35,0.85),
-#         legend.key = element_rect(fill = "white"),
-#         legend.background = element_rect(fill = "white"),
-#         panel.background = element_rect(fill = "white", color = "white"),
+# plot as points
+# fig <- ggplot(all, aes(year, records), group = id) +
+#   geom_point(aes(shape = id), size = 2) +
+#   scale_shape_manual(name = "", values = c(1,18,8,2), labels = c("Sea-Level Rise\nOR Sea Level Rise", "AND Risk", "AND Population",
+#                                                                 "AND Population AND Risk")) +
+#   # geom_smooth(method = "loess", se = FALSE, lwd = 1.2, aes(color = identity)) +
+#   # scale_color_manual(name = "", values = c("steelblue1", "tomat3"), labels = c("Vulnerability", "AND (Hazards OR Disasters)")) +
+#   scale_y_continuous(name = "No. of Records", limits = c(0,320), breaks = seq(0, 320, 20),
+#                      sec.axis = sec_axis(~., breaks = seq(0,320,20), labels = NULL)) +
+#   scale_x_continuous(name = "Year", breaks = seq(1970, 2020, 10),
+#                      sec.axis = sec_axis(~., breaks = seq(1970,2020, 10), labels = NULL)) +
+#   theme(legend.position = c(0.25,0.85),
+#         legend.key = element_rect(fill = "transparent", color = "transparent"),
+#         legend.background = element_rect(fill = "transparent"),
+#         panel.background = element_rect(fill = "transparent", color = "transparent"),
 #         panel.grid = element_blank(),
-#         plot.background = element_rect(fill = "white", color = "white"),
+#         plot.background = element_rect(fill = "transparent", color = "transparent"),
 #         axis.line = element_line(color = "black"),
 #         axis.text = element_text(color = "black"),
 #         axis.text.x = element_text(margin = unit(c(0.5,0.5,0.5,0.5), "cm")),
 #         axis.text.y = element_text(margin = unit(c(0.5,0.5,0.5,0.5), "cm")),
-#         axis.ticks.length = unit(-0.15, "cm"),
+#         axis.ticks.length = unit(-0.1, "cm"),
 #         axis.ticks = element_line(color = "black"),
-#         text = element_text(size = 12, color = "black"),
+#         text = element_text(size = 10, color = "black"),
 #         plot.margin = margin(1,1,0.5,0.5, "cm")
-#         )
+#   )
 # fig
-# 
-# tiff('figures/slr-pop-risk.tiff', width=5, height=5, units="in", res=300, compression = "lzw")
-# fig
-# dev.off()
 
-
-fig <- ggplot(all, aes(year, records), group = id) + 
-  geom_point(aes(shape = id), size = 2) +
-  scale_shape_manual(name = "", values = c(1,18,8,2), labels = c("Sea-Level Rise\nOR Sea Level Rise", "AND Risk", "AND Population",
-                                                                "AND Population AND Risk")) +
-  # geom_smooth(method = "loess", se = FALSE, lwd = 1.2, aes(color = identity)) +
-  # scale_color_manual(name = "", values = c("steelblue1", "tomat3"), labels = c("Vulnerability", "AND (Hazards OR Disasters)")) +
-  scale_y_continuous(name = "No. of Records", limits = c(0,320), breaks = seq(0, 320, 20),
-                     sec.axis = sec_axis(~., breaks = seq(0,320,20), labels = NULL)) + 
-  scale_x_continuous(name = "Year", breaks = seq(1990, 2020, 5), 
-                     sec.axis = sec_axis(~., breaks = seq(1990,2020, 5), labels = NULL)) + 
-  theme(legend.position = c(0.25,0.85),
-        legend.key = element_rect(fill = "transparent", color = "transparent"),
-        legend.background = element_rect(fill = "transparent"),
+## still working on getting columns to overlay and show legend in desired order. 
+## plot as overlaid columns
+fig_col <- ggplot() + 
+  geom_col(aes(year, records, fill = 'grey90'), filter(all, id == 'slr' & year > 1989)) + 
+  geom_col(aes(year, records, fill = 'grey60'), filter(all, id == 'risk')) + 
+  geom_col(aes(year, records, fill = 'grey30'), filter(all, id == 'pop')) + 
+  geom_col(aes(year, records, fill = 'black'), filter(all, id == 'pop+risk')) + 
+  #geom_bar(stat = "identity", position = "identity", alpha=0.9) +
+  scale_fill_manual(name = "", values = rev(c('grey90', 'grey60', 'grey30', 'black')), labels =
+                     rev(c("Sea-Level Rise\nOR Sea Level Rise", "AND Risk", "AND Population", "AND Population AND Risk"))) +
+  scale_y_continuous(name = "No. of Records", limits = c(0,1600), breaks = seq(0, 1600, 200),
+                     minor_breaks = seq(0, 1600, 100),
+                     sec.axis = sec_axis(~., breaks = seq(0,1600,200), labels = NULL),
+                     expand = c(0,0)) + 
+  scale_x_continuous(name = "Year", breaks = seq(1990, 2020, 5),
+                     sec.axis = sec_axis(~., breaks = seq(1990,2020, 5), labels = NULL)
+                     ) + 
+  theme(legend.position = c(0.3,0.8),
+        # legend.key = element_rect(fill = "transparent", color = "transparent"),
+        # legend.background = element_rect(fill = "transparent"),
         panel.background = element_rect(fill = "transparent", color = "transparent"),
         panel.grid = element_blank(),
         plot.background = element_rect(fill = "transparent", color = "transparent"),
@@ -94,10 +101,10 @@ fig <- ggplot(all, aes(year, records), group = id) +
         text = element_text(size = 10, color = "black"),
         plot.margin = margin(1,1,0.5,0.5, "cm")
   )
-fig
+fig_col
 
-tiff(file.path(datadir, '/figures/slr-pop-risk.tiff'), width=5, height=5, units="in", res = 300, compression = 'lzw')
-fig
+tiff(file.path(datadir, '/figures/slr-pop-risk-columns.tiff'), width=5, height=5, units="in", res = 300, compression = 'lzw')
+fig_col
 dev.off()
 
 
